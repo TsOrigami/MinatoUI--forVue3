c44acbf67e128587630f09a70ddabdb4a6db2e6e
TsOrigami <1306759460@qq.com>
2024-08-16 21:21:56 +0800

    ver 1.1.12

--- src/App.vue
+++ src/App.vue
@@ -13,6 +13,7 @@ import mt_switch from './components/mt-switch.vue'
 import mt_autocomplete from './components/mt-autocomplete.vue'
 import mt_checkbox from './components/mt-checkbox.vue'
 import mt_colorPicker from './components/mt-colorPicker.vue'
+import mt_slider from './components/mt_slider.vue'
 import axios from 'axios'
 
 const data = ref([])
@@ -116,6 +117,8 @@ const checkbox_test = ref()
 
 const slt_color = ref('')
 
+const mt_slider_data = ref(0)
+
 </script>
 
 <template>
@@ -146,10 +149,12 @@ const slt_color = ref('')
         <mt_checkbox v-model = "checkbox_test" style="top: 100px; left: 10px; position: absolute;" />
       <button @click="console.log(checkbox_test?'True':'False')">test</button>
     </div> -->
-     <div style="position: absolute; left: 100px; top: 100px;">
+    <!--<div style="position: absolute; left: 100px; top: 100px;">
       <mt_color-picker v-model="slt_color" RGBA></mt_color-picker>
       <button @click="console.log(slt_color)">Show</button>
-     </div>
+     </div> -->
+     <mt_slider v-model="mt_slider_data" style="position: absolute ; top: 100px; left: 50px;"></mt_slider>
+     <button @click="console.log(mt_slider_data)">test</button>
   </div>
   <!-- <div class="test"></div> -->
 </template>
---
+++ src/components/mt_slider.vue
@@ -0,0 +1,112 @@
+<script setup>
+
+////1.组件名称：mt-slider
+//2.组件功能：滑块
+//3.组件参数：
+//      width: Number，宽度，默认200
+//      height: Number，高度，默认10
+//      color: String，滑块颜色，默认gray
+//      sltColor: String，滑块选中部分颜色 / 滑块浮标边框颜色，默认skyblue
+//      双向绑定参数: v-model: Number, 选中部分占比，范围为0-100，保留整数
+
+import {ref, defineProps, defineModel, onMounted, watch} from 'vue'
+
+const mt_slider_data = defineProps({
+    'width':{
+        type: Number,
+        default: 200
+    },
+    'height':{
+        type:Number,
+        default: 10
+    },
+    'color':{
+        type: String,
+        default: 'gray'
+    },
+    'sltColor':{
+        type: String,
+        default: 'skyblue'
+    }
+})
+
+const mt_slider_sltData = defineModel({
+    type: Number,
+    default: 0
+})
+
+const mt_slider_length = ref(0)
+
+const mt_slider_pushSilder = () =>{
+    const body = document.body
+    body.addEventListener('mousemove',mt_slider_moveSilder)
+    body.addEventListener('mouseup', mt_slider_freeSilder)
+}
+
+const mt_slider_moveSilder = (event) =>{
+    let pos = event.x
+    let len = document.getElementById('mt_slider_main').offsetLeft + mt_slider_data['height'] / 2 
+    mt_slider_length.value = ((pos - len) / (mt_slider_data['width'] - mt_slider_data['height'])) * 100
+    if(mt_slider_length.value > 100){
+        mt_slider_length.value = 100
+    } else if(mt_slider_length.value < 0){
+        mt_slider_length.value = 0
+    }
+    mt_slider_sltData.value = Number(mt_slider_length.value.toFixed(0))
+}
+
+const mt_slider_freeSilder = () =>{
+    const body = document.body
+    body.removeEventListener('mousemove',mt_slider_moveSilder)
+    body.removeEventListener('mouseup', mt_slider_freeSilder)
+}
+
+const mt_slider_moveToSilder = (event) =>{
+    let pos = event.x
+    let len = document.getElementById('mt_slider_main').offsetLeft + mt_slider_data['height'] / 2 
+    mt_slider_length.value = ((pos - len) / (mt_slider_data['width'] - mt_slider_data['height'])) * 100
+    if(mt_slider_length.value > 100){
+        mt_slider_length.value = 100
+    } else if(mt_slider_length.value < 0){
+        mt_slider_length.value = 0
+    }
+    mt_slider_sltData.value = Number(mt_slider_length.value.toFixed(0))
+}
+
+</script>
+
+<template>
+    <div id="mt_slider_main">
+        <div id="mt_slider_line" 
+        @mousedown="mt_slider_moveToSilder"
+        style="position: relative; border: 2px solid black;"
+        :style="{
+            backgroundColor: mt_slider_data['color'],
+            width: mt_slider_data['width'] + 'px',
+            height: mt_slider_data['height'] + 'px',
+            borderRadius: mt_slider_data['height'] / 2 +'px',
+        }">
+            <div id="mt_slider_isColor" style="position: absolute;"
+                :style="{
+                    backgroundColor: mt_slider_data['sltColor'],
+                    height: mt_slider_data['height'] - 4 +'px',
+                    width: mt_slider_length / 100 * (mt_slider_data['width'] - 4) + 'px',
+                    borderRadius: (mt_slider_data['height'] - 4) / 2 +'px',
+                }"></div>
+        </div>
+        <div id="mt_slider_bobber" style="position: absolute; cursor: move; background-color: white; border-style: solid; border-width: 2px;"
+            :style="{
+                borderColor: mt_slider_data['sltColor'],
+                height: mt_slider_data['height'] * 2 + 'px',
+                width: mt_slider_data['height'] * 2 + 'px',
+                borderRadius: mt_slider_data['height'] + 'px',
+                top: - mt_slider_data['height'] * 0.5 +'px',
+                left: - mt_slider_data['height'] / 2 + mt_slider_length / 100 * (mt_slider_data['width']  - mt_slider_data['height']) +'px'
+            }"
+            @mousedown = "mt_slider_pushSilder"></div>
+    </div>
+</template>
+
+<style>
+
+</style>
